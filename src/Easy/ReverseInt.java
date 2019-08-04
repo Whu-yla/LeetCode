bdf0710a6a98d639bb2ab94d7eddbb81c9dc00a8
Hugo <274215367@qq.com>
2019-08-04 10:04:22 +0800

    Initial commit

---
+++ src/Easy/ReverseInt.java
@@ -0,0 +1,67 @@
+package Easy;
+
+/**
+ * create by: Hugo
+ * description:本人的转置int就很简单，思路看下面的注释
+ * create time: 9:54 2019/8/4
+ * @Param: x
+ * @return int
+ */
+public class ReverseInt {
+    public static int myReverse(int x) {
+        int result = 0;// 定义一个全局的返回值，默认是0
+        if (Math.pow(-2, 31) <= x && x <= (Math.pow(2, 31) - 1)) {// 判断输入是否超限
+            String s = String.valueOf(x);// 将输入的int转成string
+            if (s.indexOf("-") != -1) {// 判断一下string中能否找到负号，及判断输入是否为负数
+                s = s.substring(1, s.length());// 取出负数之后的字符串
+                char[] chars = s.toCharArray();// 转成char[]
+                char[] temp = new char[s.length()];// 创造一个装字符的字符数组
+                for (int i = 0; i < s.length(); i++) {// 开始遍历
+                    temp[i] = chars[s.length() - 1 - i];// 开始遍历转置
+                }
+                String result2 = "-" + String.valueOf(temp);// 加上符号准备输出
+                try {
+                    result = Integer.parseInt(result2);// 如果try没有捕获异常就表明转置之后也在int范围之内
+                } catch (NumberFormatException e) {// 捕获异常，输出result=0
+                    result = 0;
+                }
+            } else {// esle表明输入为正整数，即不需要符号，其余思路与上述相同。
+                char[] chars = s.toCharArray();
+                char[] temp = new char[s.length()];
+                for (int i = 0; i < s.length(); i++) {
+                    temp[i] = chars[s.length() - 1 - i];
+                }
+                String result2 = String.valueOf(temp);
+                try {
+                    result = Integer.parseInt(result2);
+                } catch (NumberFormatException e) {
+                    result = 0;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * create by: Hugo
+     * description:排名第一的人的解法
+     * create time: 9:53 2019/8/4
+     *
+     * @return
+     * @Param: null
+     */
+    public int reverse(int x) {
+        int mus = 0;
+        int dao = 0;
+        if (x == 0)
+            return 0;
+        while (x != 0) {
+            mus = x % 10;
+            x = x / 10;
+            if (dao > Integer.MAX_VALUE / 10 || dao < Integer.MIN_VALUE / 10)
+                return 0;
+            dao = dao * 10 + mus;
+        }
+        return dao;
+    }
+}
---
+++ src/com/Hugo/Main.java
@@ -0,0 +1,11 @@
+package com.Hugo;
+
+import Easy.ReverseInt;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 1534236469;
+        System.out.println(ReverseInt.reverse(x));
+    }
+}
